84950529b8b669b2ef1c8885190a0ba5ec113c31
Olha Danylchenko <danylchenkoolha@gmail.com>
2020-04-27 18:54:06 +0200

    scraped 114 plants

---
+++ seed/seeds.js
@@ -0,0 +1,15 @@
+// const mongoose = require("mongoose");
+// const Movie = require("../models/Movie");
+
+// mongoose.connect("mongodb://localhost/plants", {
+//   useNewUrlParser: true,
+// });
+
+// Plant.insertMany(plants)
+//   .then((data) => {
+//     console.log("Success");
+//     mongoose.connection.close();
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
