443fbffddcc5e67f104595720a0bb4f632c2acc4
emelko <ml.kochsiek@mailbox.org>
2020-04-30 15:12:22 +0200

    WIP last Watered

--- routes/dashboard/profile.js
+++ routes/dashboard/profile.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const UserPlant = require("../../models/UserPlant");
+const moment = require("moment")
 
 // middleware that checks if a user is logged in
 const loginCheck = () => {
@@ -17,29 +18,31 @@ const loginCheck = () => {
 
 router.get('/', loginCheck(), (req, res) => {
     const user = req.user
+    // get waterSchedule and lastWater and calculate nextWater 
+    function mapwaterScheduleToNumOfDays(waterSchedule) {
+      const objectForwaterSchedule = {
+        'Daily': 1,
+        'Every other day': 2,
+        'Twice a week': 3,
+        'Weekly': 7,
+        'Biweekly': 14,
+        'Monthly': 30,
+      }
+      return objectForwaterSchedule[waterSchedule]
+    }
+    const today = moment().format('YYYY-MM-DD')
     const allPlants = req.user.plantCollection
     const allPlantsPromise = allPlants.map(idOfUserPlant => {
       return Promise.resolve(UserPlant.findById(idOfUserPlant))
     })
     Promise.all(allPlantsPromise).then(userplants => {
+      userplants.map(userplant => {
+        const numberOfDaysForWateringRoutine = mapwaterScheduleToNumOfDays(userplant.waterSchedule)
+        const nextWater = moment(userplant.lastWater).add(numberOfDaysForWateringRoutine, 'days').format('YYYY-MM-DD')
+        return nextWater
+      })
       res.render("dashboard/profile", { user: user, userplants })
     })
   });  
-    // // get waterSchedule and lastWater and calculate nextWater 
-    // function mapwaterScheduleToNumOfDays(waterSchedule) {
-    //   const objectForwaterSchedule = {
-    //     'Daily': 1,
-    //     'Every other day': 2,
-    //     'Twice a week': 3,
-    //     'Weekly': 7,
-    //     'Biweekly': 14,
-    //     'Monthly': 30,
-    //   }
-    //   return objectForwaterSchedule[waterSchedule]
-    // }
-    
-    // const numberOfDaysForWateringRoutine = mapwaterScheduleToNumOfDays("Weekly")
-    // const lastWater = "YYYY-MM-DD"
-    // const nextWater = lastWater.add(numberOfDaysForWateringRoutine, 'days')
 
 module.exports = router;
