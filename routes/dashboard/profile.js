950c0ad5739b73ba2a0f23b53d78365ac2ce9785
Olha Danylchenko <danylchenkoolha@gmail.com>
2020-04-30 12:56:02 +0200

    dashboard first commit

--- routes/dashboard/profile.js
+++ routes/dashboard/profile.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const Plant = require('../../models/Plant');
-const UserPlant = require('../../models/UserPlant');
-const User = require('../../models/User');
-
+const Plant = require("../../models/Plant");
+const UserPlant = require("../../models/UserPlant");
+const User = require("../../models/User");
 
 // middleware that checks if a user is logged in
 const loginCheck = () => {
@@ -12,20 +11,16 @@ const loginCheck = () => {
     if (req.isAuthenticated()) {
       next();
     } else {
-      res.redirect('/auth/login');
+      res.redirect("/auth/login");
     }
   };
 };
 
-
-router.get('/', loginCheck(), (req, res) => {
-
-
-    const user = req.user;
-    res.render('dashboard/profile', { user: user} );
-
-
+router.get("/", loginCheck(), (req, res) => {
+  const user = req.user;
+  UserPlant.find().then((plants) => {
+    res.render("dashboard/profile", { user, plants });
   });
+});
 
-
-  module.exports = router;
+module.exports = router;
