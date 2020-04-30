e0387caa564f8ad9e15d6e4df6e229783473d237
emelko <ml.kochsiek@mailbox.org>
2020-04-30 12:01:34 +0200

    Adds watering info for plant collection to profile

--- routes/dashboard/profile.js
+++ routes/dashboard/profile.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Plant = require("../../models/Plant");
 const UserPlant = require("../../models/UserPlant");
-const User = require("../../models/User");
 
 // middleware that checks if a user is logged in
 const loginCheck = () => {
@@ -16,11 +14,32 @@ const loginCheck = () => {
   };
 };
 
-router.get("/", loginCheck(), (req, res) => {
-  const user = req.user;
-  UserPlant.find().then((plants) => {
-    res.render("dashboard/profile", { user, plants });
-  });
-});
+
+router.get('/', loginCheck(), (req, res) => {
+    const user = req.user
+    const allPlants = req.user.plantCollection
+    const allPlantsPromise = allPlants.map(idOfUserPlant => {
+      return Promise.resolve(UserPlant.findById(idOfUserPlant))
+    })
+    Promise.all(allPlantsPromise).then(userplants => {
+      res.render("dashboard/profile", { user: user, userplants })
+    })
+  });  
+    // // get waterSchedule and lastWater and calculate nextWater 
+    // function mapwaterScheduleToNumOfDays(waterSchedule) {
+    //   const objectForwaterSchedule = {
+    //     'Daily': 1,
+    //     'Every other day': 2,
+    //     'Twice a week': 3,
+    //     'Weekly': 7,
+    //     'Biweekly': 14,
+    //     'Monthly': 30,
+    //   }
+    //   return objectForwaterSchedule[waterSchedule]
+    // }
+    
+    // const numberOfDaysForWateringRoutine = mapwaterScheduleToNumOfDays("Weekly")
+    // const lastWater = "YYYY-MM-DD"
+    // const nextWater = lastWater.add(numberOfDaysForWateringRoutine, 'days')
 
 module.exports = router;
