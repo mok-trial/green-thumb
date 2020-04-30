30c533a130c7b54b6b9f98095356a2e5258432a8
Olha Danylchenko <danylchenkoolha@gmail.com>
2020-04-30 10:09:03 +0200

    fixed the pic logic
    Co-authored-by: karlkristopher <karl.secen@gmail.com>

--- routes/addPlant/addDBPlant.js
+++ routes/addPlant/addDBPlant.js
@@ -60,42 +60,45 @@ router.post(
   uploadCloud.single("photo"),
   loginCheck(),
   (req, res) => {
+    const userId = req.user._id;
     const { customName, waterSchedule, lastWater, notes } = req.body;
     const plantInfo = req.params.plantId;
 
-    const defaultUserImage =
-      "https://res.cloudinary.com/rootdirectory/image/upload/v1588166094/user-plants/rootdir-assets/default-plant.png";
-    let imgPath = req.file ? req.file.url : defaultUserImage;
-    let imgName = req.file ? req.file.originalname : "default-image";
+    Plant.findById(req.params.plantId)
+      .then((plant) => {
+        console.log(plant.image);
 
-    const userId = req.user._id;
+        let imgPath = req.file ? req.file.url : plant.image;
+        let imgName = req.file ? req.file.originalname : "database-image";
 
-    UserPlant.create({
-      customName,
-      waterSchedule,
-      notes,
-      plantInfo,
-      imgName,
-      imgPath,
-      lastWater,
-    })
+        UserPlant.create({
+          customName,
+          waterSchedule,
+          notes,
+          plantInfo,
+          imgName,
+          imgPath,
+          lastWater,
+        })
 
-      .then((userPlant) => {
-        console.log("plant added");
-        User.updateOne(
-          { _id: userId },
-          { $push: { plantCollection: userPlant } }
-        ).catch((error) => {
-          console.log(error);
-        });
+          .then((userPlant) => {
+            console.log("plant added");
+            User.updateOne(
+              { _id: userId },
+              { $push: { plantCollection: userPlant } }
+            ).catch((error) => {
+              console.log(error);
+            });
+          })
+
+          .then((data) => {
+            console.log(`Success ${data} was added to the database`);
+
+            res.redirect("/profile");
+          })
+          .catch((err) => res.render("addPlant/addPlantFromDB"));
       })
-
-      .then((data) => {
-        console.log(`Success ${data} was added to the database`);
-
-        res.redirect("/profile");
-      })
-      .catch((err) => res.render("addPlant/addPlantFromDB"));
+      .catch((err) => console.log(err));
   }
 );
 
